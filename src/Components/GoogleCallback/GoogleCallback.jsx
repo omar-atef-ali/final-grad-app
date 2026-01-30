aba7bfac3848317defb535c019195db03aa16d3a
omar-atef-ali <oatef266@gmail.com>
2026-01-30 20:59:37 +0200

    googlee

--- src/Components/GoogleCallback/GoogleCallback.jsx
+++ src/Components/GoogleCallback/GoogleCallback.jsx
@@ -17,28 +17,28 @@ export default function GoogleCallback() {
 
       console.log(code);
 
-      try {
-        const res = await api.post("/Auth/google", {
-          code,
-          redirectUri: "https://finalgradapp.netlify.app/google/callback",
-        });
+      // try {
+      //   const res = await api.post("/Auth/google", {
+      //     code,
+      //     redirectUri: "https://finalgradapp.netlify.app/google/callback",
+      //   });
 
-        const data = res.data;
-        console.log(data);
+      //   const data = res.data;
+      //   console.log(data);
         
-        // if (data.token) {
-        //   localStorage.setItem("token", data.token);
-        //   if (data.refreshToken) {
-        //     localStorage.setItem("refreshToken", data.refreshToken);
-        //   }
-        //   console.log(data.token);
+      //   // if (data.token) {
+      //   //   localStorage.setItem("token", data.token);
+      //   //   if (data.refreshToken) {
+      //   //     localStorage.setItem("refreshToken", data.refreshToken);
+      //   //   }
+      //   //   console.log(data.token);
           
-        //   setUserToken(data.token);
-        //   navigate("/home");
-        // }
-      } catch (error) {
-        console.error("Google login error:", error);
-      }
+      //   //   setUserToken(data.token);
+      //   //   navigate("/home");
+      //   // }
+      // } catch (error) {
+      //   console.error("Google login error:", error);
+      // }
     };
 
     handleGoogleCallback();
