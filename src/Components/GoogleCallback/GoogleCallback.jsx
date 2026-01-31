e359cee7d2b277ab35f110b0d850a1100e1ff34f
omar-atef-ali <oatef266@gmail.com>
2026-01-31 03:02:08 +0200

    final  googlee

--- src/Components/GoogleCallback/GoogleCallback.jsx
+++ src/Components/GoogleCallback/GoogleCallback.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import style from "./GoogleCallback.module.css";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { data, useNavigate } from "react-router-dom";
 import api from "../../api";
 import { userContext } from "../../context/userContext";
+import toast from "react-hot-toast";
 
 export default function GoogleCallback() {
   const navigate = useNavigate();
   const { setUserToken } = useContext(userContext);
+
+      const [pageLoading, setPageLaoding] = useState(false);
+
 
   useEffect(() => {
     const handleGoogleCallback = async () => {
@@ -17,32 +21,81 @@ export default function GoogleCallback() {
 
       console.log(code);
 
-      // try {
-      //   const res = await api.post("/Auth/google", {
-      //     code,
-      //     redirectUri: "https://finalgradapp.netlify.app/google/callback",
-      //   });
+      try {
+        setPageLaoding(true)
+        const res = await api.post("/Auth/google/login", {
+          code,
+          redirectUri: "https://finalgradapp.netlify.app/google/callback",
+        });
 
-      //   const data = res.data;
-      //   console.log(data);
+        const data = res.data;
+        console.log(data);
         
-      //   // if (data.token) {
-      //   //   localStorage.setItem("token", data.token);
-      //   //   if (data.refreshToken) {
-      //   //     localStorage.setItem("refreshToken", data.refreshToken);
-      //   //   }
-      //   //   console.log(data.token);
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          if (data.refreshToken) {
+            localStorage.setItem("refreshToken", data.refreshToken);
+          }
+          console.log(data.token);
           
-      //   //   setUserToken(data.token);
-      //   //   navigate("/home");
-      //   // }
-      // } catch (error) {
-      //   console.error("Google login error:", error);
-      // }
+          setUserToken(data.token);
+          navigate("/home");
+          // setPageLaoding(false)
+
+        }
+      } catch (error) {
+        // console.error("Google login error:", error.response?.data?.errors[1]);
+        toast.error(
+        error.response?.data?.errors[1] ||
+        "Something went wrong while login.",
+        {
+          position: "top-center",
+          duration: 4000,
+          style: {
+            background:
+              "linear-gradient(to right, rgba(121, 5, 5, 0.9), rgba(171, 0, 0, 0.85))",
+            border: "1px solid rgba(255, 255, 255, 0.1)",
+            padding: "16px 20px",
+            color: "#ffffff",
+            fontSize: "0.95rem",
+            borderRadius: "5px",
+            width: "300px",
+            height: "60px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.5)",
+          },
+          iconTheme: {
+            primary: "#FF4D4F",
+            secondary: "#ffffff",
+          },
+        },
+        { id: "google-login-error" }
+      );
+      setTimeout(() => {
+        navigate("/register");
+      }, 1000);
+    } 
     };
 
     handleGoogleCallback();
   }, [navigate]);
 
-  return <p>Signing in with Google...</p>;
+
+
+
+  return <>
+  
+  {/* {pageLoading ? (
+          <div className={style.overlay}>
+            <div className={style.spinner}></div>
+          </div>
+        ) : ("")} */}
+
+
+      
+      <div className={style.callback}>
+        <p>loading....</p>
+      </div>
+  
+  </>;
 }
+// 
